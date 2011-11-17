9509b6643a1abace97e7a214ed33bd83522eeebd
matthew90 <matthew90@37f40823-ce63-ffd7-518b-180745758533>
2011-11-17 23:08:52 +0000

    changed stuff up

--- hw2_pass/lib/sample/Debug/sample.d
+++
@@ -1,6 +0,0 @@
-/y/students/jpvarbed/hw1/samplepass/lib/sample/Debug/sample.o \
-  /y/students/jpvarbed/hw1/samplepass/lib/sample/Debug/sample.d \
-  /y/students/jpvarbed/hw1/samplepass/lib/sample/Debug/sample.o:  \
- sample.c /y/students/jpvarbed/hw1/samplepass/include/sample.h
-
-/y/students/jpvarbed/hw1/samplepass/include/sample.h:

2798312fcaefc84207af6640d60a6faf2a1ed8c5
jpvarbed <jpvarbed@37f40823-ce63-ffd7-518b-180745758533>
2011-11-17 18:25:09 +0000

    added my hw2 as a start structure

---
+++ trunk/hw2_pass/lib/hw1pass/Release+Asserts/hw1lib.d
@@ -0,0 +1,315 @@
+/y/students/jpvarbed/hw1/lamppass/lib/hw1pass/Release+Asserts/hw1lib.o \
+  /y/students/jpvarbed/hw1/lamppass/lib/hw1pass/Release+Asserts/hw1lib.d \
+  /y/students/jpvarbed/hw1/lamppass/lib/hw1pass/Release+Asserts/hw1lib.o:  \
+ hw1lib.cpp /y/students/jpvarbed/llvm-2.9/include/llvm/Pass.h \
+  /y/students/jpvarbed/llvm-2.9/include/llvm/PassSupport.h \
+  /y/students/jpvarbed/llvm-2.9/include/llvm/Pass.h \
+  /y/students/jpvarbed/llvm-2.9/include/llvm/PassRegistry.h \
+  /y/students/jpvarbed/llvm-2.9/include/llvm/ADT/StringRef.h \
+  /y/students/jpvarbed/llvm-2.9/include/llvm/InitializePasses.h \
+  /y/students/jpvarbed/llvm-2.9/include/llvm/Support/Atomic.h \
+  /y/students/jpvarbed/llvm-object/include/llvm/Support/DataTypes.h \
+  /y/students/jpvarbed/llvm-2.9/include/llvm/PassAnalysisSupport.h \
+  /y/students/jpvarbed/llvm-2.9/include/llvm/ADT/SmallVector.h \
+  /y/students/jpvarbed/llvm-2.9/include/llvm/Support/type_traits.h \
+  /y/students/jpvarbed/llvm-2.9/include/llvm/Function.h \
+  /y/students/jpvarbed/llvm-2.9/include/llvm/GlobalValue.h \
+  /y/students/jpvarbed/llvm-2.9/include/llvm/Constant.h \
+  /y/students/jpvarbed/llvm-2.9/include/llvm/User.h \
+  /y/students/jpvarbed/llvm-2.9/include/llvm/Value.h \
+  /y/students/jpvarbed/llvm-2.9/include/llvm/AbstractTypeUser.h \
+  /y/students/jpvarbed/llvm-2.9/include/llvm/Use.h \
+  /y/students/jpvarbed/llvm-2.9/include/llvm/ADT/PointerIntPair.h \
+  /y/students/jpvarbed/llvm-2.9/include/llvm/Support/PointerLikeTypeTraits.h \
+  /y/students/jpvarbed/llvm-2.9/include/llvm/Support/Casting.h \
+  /y/students/jpvarbed/llvm-2.9/include/llvm/CallingConv.h \
+  /y/students/jpvarbed/llvm-2.9/include/llvm/BasicBlock.h \
+  /y/students/jpvarbed/llvm-2.9/include/llvm/Instruction.h \
+  /y/students/jpvarbed/llvm-2.9/include/llvm/ADT/ilist_node.h \
+  /y/students/jpvarbed/llvm-2.9/include/llvm/Support/DebugLoc.h \
+  /y/students/jpvarbed/llvm-2.9/include/llvm/Instruction.def \
+  /y/students/jpvarbed/llvm-2.9/include/llvm/SymbolTableListTraits.h \
+  /y/students/jpvarbed/llvm-2.9/include/llvm/ADT/ilist.h \
+  /y/students/jpvarbed/llvm-2.9/include/llvm/ADT/Twine.h \
+  /y/students/jpvarbed/llvm-2.9/include/llvm/ADT/StringRef.h \
+  /y/students/jpvarbed/llvm-object/include/llvm/Support/DataTypes.h \
+  /y/students/jpvarbed/llvm-object/include/llvm/Support/DataTypes.h \
+  /y/students/jpvarbed/llvm-2.9/include/llvm/Argument.h \
+  /y/students/jpvarbed/llvm-2.9/include/llvm/Attributes.h \
+  /y/students/jpvarbed/llvm-2.9/include/llvm/Support/MathExtras.h \
+  /y/students/jpvarbed/llvm-2.9/include/llvm/Support/SwapByteOrder.h \
+  /y/students/jpvarbed/llvm-2.9/include/llvm/Support/Compiler.h \
+  /y/students/jpvarbed/llvm-2.9/include/llvm/Support/raw_ostream.h \
+  /y/students/jpvarbed/llvm-2.9/include/llvm/ADT/StringRef.h \
+  /y/students/jpvarbed/llvm-2.9/include/llvm/Analysis/ProfileInfo.h \
+  /y/students/jpvarbed/llvm-2.9/include/llvm/Support/Debug.h \
+  /y/students/jpvarbed/llvm-2.9/include/llvm/Support/Format.h \
+  /y/students/jpvarbed/llvm-2.9/include/llvm/Support/raw_ostream.h \
+  /y/students/jpvarbed/hw1/lamppass/include/LAMPLoadProfile.h \
+  /y/students/jpvarbed/llvm-2.9/include/llvm/Module.h \
+  /y/students/jpvarbed/llvm-2.9/include/llvm/Function.h \
+  /y/students/jpvarbed/llvm-2.9/include/llvm/GlobalVariable.h \
+  /y/students/jpvarbed/llvm-2.9/include/llvm/OperandTraits.h \
+  /y/students/jpvarbed/llvm-2.9/include/llvm/GlobalAlias.h \
+  /y/students/jpvarbed/llvm-2.9/include/llvm/Metadata.h \
+  /y/students/jpvarbed/llvm-2.9/include/llvm/ADT/ArrayRef.h \
+  /y/students/jpvarbed/llvm-2.9/include/llvm/ADT/SmallVector.h \
+  /y/students/jpvarbed/llvm-2.9/include/llvm/ADT/FoldingSet.h \
+  /y/students/jpvarbed/llvm-2.9/include/llvm/ADT/OwningPtr.h \
+  /y/students/jpvarbed/llvm-2.9/include/llvm/Pass.h \
+  /y/students/jpvarbed/llvm-2.9/include/llvm/Analysis/Passes.h \
+  /y/students/jpvarbed/llvm-2.9/include/llvm/Analysis/LoopPass.h \
+  /y/students/jpvarbed/llvm-2.9/include/llvm/Analysis/LoopInfo.h \
+  /y/students/jpvarbed/llvm-2.9/include/llvm/Pass.h \
+  /y/students/jpvarbed/llvm-2.9/include/llvm/ADT/DenseMap.h \
+  /y/students/jpvarbed/llvm-2.9/include/llvm/Support/MathExtras.h \
+  /y/students/jpvarbed/llvm-2.9/include/llvm/ADT/DenseMapInfo.h \
+  /y/students/jpvarbed/llvm-2.9/include/llvm/ADT/DepthFirstIterator.h \
+  /y/students/jpvarbed/llvm-2.9/include/llvm/ADT/GraphTraits.h \
+  /y/students/jpvarbed/llvm-2.9/include/llvm/ADT/SmallPtrSet.h \
+  /y/students/jpvarbed/llvm-2.9/include/llvm/ADT/PointerIntPair.h \
+  /y/students/jpvarbed/llvm-2.9/include/llvm/ADT/GraphTraits.h \
+  /y/students/jpvarbed/llvm-2.9/include/llvm/ADT/SmallVector.h \
+  /y/students/jpvarbed/llvm-2.9/include/llvm/ADT/STLExtras.h \
+  /y/students/jpvarbed/llvm-2.9/include/llvm/Analysis/Dominators.h \
+  /y/students/jpvarbed/llvm-2.9/include/llvm/Function.h \
+  /y/students/jpvarbed/llvm-2.9/include/llvm/ADT/SmallPtrSet.h \
+  /y/students/jpvarbed/llvm-2.9/include/llvm/Support/CFG.h \
+  /y/students/jpvarbed/llvm-2.9/include/llvm/ADT/GraphTraits.h \
+  /y/students/jpvarbed/llvm-2.9/include/llvm/Function.h \
+  /y/students/jpvarbed/llvm-2.9/include/llvm/InstrTypes.h \
+  /y/students/jpvarbed/llvm-2.9/include/llvm/Operator.h \
+  /y/students/jpvarbed/llvm-2.9/include/llvm/Constants.h \
+  /y/students/jpvarbed/llvm-2.9/include/llvm/ADT/APInt.h \
+  /y/students/jpvarbed/llvm-2.9/include/llvm/ADT/APFloat.h \
+  /y/students/jpvarbed/llvm-2.9/include/llvm/ADT/APInt.h \
+  /y/students/jpvarbed/llvm-2.9/include/llvm/DerivedTypes.h \
+  /y/students/jpvarbed/llvm-2.9/include/llvm/Type.h \
+  /y/students/jpvarbed/llvm-2.9/include/llvm/ADT/GraphTraits.h \
+  /y/students/jpvarbed/llvm-2.9/include/llvm/Support/Compiler.h \
+  /y/students/jpvarbed/llvm-2.9/include/llvm/PassManagers.h \
+  /y/students/jpvarbed/llvm-2.9/include/llvm/Pass.h \
+  /y/students/jpvarbed/llvm-2.9/include/llvm/ADT/SmallPtrSet.h \
+  /y/students/jpvarbed/llvm-2.9/include/llvm/ADT/DenseMap.h \
+  /y/students/jpvarbed/llvm-2.9/include/llvm/Support/PrettyStackTrace.h \
+  /y/students/jpvarbed/llvm-2.9/include/llvm/Analysis/LoopInfo.h \
+  /y/students/jpvarbed/llvm-2.9/include/llvm/Constants.h \
+  /y/students/jpvarbed/llvm-2.9/include/llvm/Instructions.h \
+  /y/students/jpvarbed/llvm-2.9/include/llvm/InstrTypes.h \
+  /y/students/jpvarbed/llvm-2.9/include/llvm/Function.h \
+  /y/students/jpvarbed/llvm-2.9/include/llvm/BasicBlock.h \
+  /y/students/jpvarbed/llvm-2.9/include/llvm/Support/Debug.h \
+  /y/students/jpvarbed/llvm-2.9/include/llvm/Target/TargetData.h \
+  /y/students/jpvarbed/llvm-2.9/include/llvm/Pass.h \
+  /y/students/jpvarbed/llvm-2.9/include/llvm/ADT/SmallVector.h \
+  /y/students/jpvarbed/llvm-object/include/llvm/Support/DataTypes.h \
+  /y/students/jpvarbed/llvm-2.9/include/llvm/ADT/IndexedMap.h \
+  /y/students/jpvarbed/hw1/lamppass/include/LAMPProfiling.h
+
+/y/students/jpvarbed/llvm-2.9/include/llvm/Pass.h:
+
+/y/students/jpvarbed/llvm-2.9/include/llvm/PassSupport.h:
+
+/y/students/jpvarbed/llvm-2.9/include/llvm/Pass.h:
+
+/y/students/jpvarbed/llvm-2.9/include/llvm/PassRegistry.h:
+
+/y/students/jpvarbed/llvm-2.9/include/llvm/ADT/StringRef.h:
+
+/y/students/jpvarbed/llvm-2.9/include/llvm/InitializePasses.h:
+
+/y/students/jpvarbed/llvm-2.9/include/llvm/Support/Atomic.h:
+
+/y/students/jpvarbed/llvm-object/include/llvm/Support/DataTypes.h:
+
+/y/students/jpvarbed/llvm-2.9/include/llvm/PassAnalysisSupport.h:
+
+/y/students/jpvarbed/llvm-2.9/include/llvm/ADT/SmallVector.h:
+
+/y/students/jpvarbed/llvm-2.9/include/llvm/Support/type_traits.h:
+
+/y/students/jpvarbed/llvm-2.9/include/llvm/Function.h:
+
+/y/students/jpvarbed/llvm-2.9/include/llvm/GlobalValue.h:
+
+/y/students/jpvarbed/llvm-2.9/include/llvm/Constant.h:
+
+/y/students/jpvarbed/llvm-2.9/include/llvm/User.h:
+
+/y/students/jpvarbed/llvm-2.9/include/llvm/Value.h:
+
+/y/students/jpvarbed/llvm-2.9/include/llvm/AbstractTypeUser.h:
+
+/y/students/jpvarbed/llvm-2.9/include/llvm/Use.h:
+
+/y/students/jpvarbed/llvm-2.9/include/llvm/ADT/PointerIntPair.h:
+
+/y/students/jpvarbed/llvm-2.9/include/llvm/Support/PointerLikeTypeTraits.h:
+
+/y/students/jpvarbed/llvm-2.9/include/llvm/Support/Casting.h:
+
+/y/students/jpvarbed/llvm-2.9/include/llvm/CallingConv.h:
+
+/y/students/jpvarbed/llvm-2.9/include/llvm/BasicBlock.h:
+
+/y/students/jpvarbed/llvm-2.9/include/llvm/Instruction.h:
+
+/y/students/jpvarbed/llvm-2.9/include/llvm/ADT/ilist_node.h:
+
+/y/students/jpvarbed/llvm-2.9/include/llvm/Support/DebugLoc.h:
+
+/y/students/jpvarbed/llvm-2.9/include/llvm/Instruction.def:
+
+/y/students/jpvarbed/llvm-2.9/include/llvm/SymbolTableListTraits.h:
+
+/y/students/jpvarbed/llvm-2.9/include/llvm/ADT/ilist.h:
+
+/y/students/jpvarbed/llvm-2.9/include/llvm/ADT/Twine.h:
+
+/y/students/jpvarbed/llvm-2.9/include/llvm/ADT/StringRef.h:
+
+/y/students/jpvarbed/llvm-object/include/llvm/Support/DataTypes.h:
+
+/y/students/jpvarbed/llvm-object/include/llvm/Support/DataTypes.h:
+
+/y/students/jpvarbed/llvm-2.9/include/llvm/Argument.h:
+
+/y/students/jpvarbed/llvm-2.9/include/llvm/Attributes.h:
+
+/y/students/jpvarbed/llvm-2.9/include/llvm/Support/MathExtras.h:
+
+/y/students/jpvarbed/llvm-2.9/include/llvm/Support/SwapByteOrder.h:
+
+/y/students/jpvarbed/llvm-2.9/include/llvm/Support/Compiler.h:
+
+/y/students/jpvarbed/llvm-2.9/include/llvm/Support/raw_ostream.h:
+
+/y/students/jpvarbed/llvm-2.9/include/llvm/ADT/StringRef.h:
+
+/y/students/jpvarbed/llvm-2.9/include/llvm/Analysis/ProfileInfo.h:
+
+/y/students/jpvarbed/llvm-2.9/include/llvm/Support/Debug.h:
+
+/y/students/jpvarbed/llvm-2.9/include/llvm/Support/Format.h:
+
+/y/students/jpvarbed/llvm-2.9/include/llvm/Support/raw_ostream.h:
+
+/y/students/jpvarbed/hw1/lamppass/include/LAMPLoadProfile.h:
+
+/y/students/jpvarbed/llvm-2.9/include/llvm/Module.h:
+
+/y/students/jpvarbed/llvm-2.9/include/llvm/Function.h:
+
+/y/students/jpvarbed/llvm-2.9/include/llvm/GlobalVariable.h:
+
+/y/students/jpvarbed/llvm-2.9/include/llvm/OperandTraits.h:
+
+/y/students/jpvarbed/llvm-2.9/include/llvm/GlobalAlias.h:
+
+/y/students/jpvarbed/llvm-2.9/include/llvm/Metadata.h:
+
+/y/students/jpvarbed/llvm-2.9/include/llvm/ADT/ArrayRef.h:
+
+/y/students/jpvarbed/llvm-2.9/include/llvm/ADT/SmallVector.h:
+
+/y/students/jpvarbed/llvm-2.9/include/llvm/ADT/FoldingSet.h:
+
+/y/students/jpvarbed/llvm-2.9/include/llvm/ADT/OwningPtr.h:
+
+/y/students/jpvarbed/llvm-2.9/include/llvm/Pass.h:
+
+/y/students/jpvarbed/llvm-2.9/include/llvm/Analysis/Passes.h:
+
+/y/students/jpvarbed/llvm-2.9/include/llvm/Analysis/LoopPass.h:
+
+/y/students/jpvarbed/llvm-2.9/include/llvm/Analysis/LoopInfo.h:
+
+/y/students/jpvarbed/llvm-2.9/include/llvm/Pass.h:
+
+/y/students/jpvarbed/llvm-2.9/include/llvm/ADT/DenseMap.h:
+
+/y/students/jpvarbed/llvm-2.9/include/llvm/Support/MathExtras.h:
+
+/y/students/jpvarbed/llvm-2.9/include/llvm/ADT/DenseMapInfo.h:
+
+/y/students/jpvarbed/llvm-2.9/include/llvm/ADT/DepthFirstIterator.h:
+
+/y/students/jpvarbed/llvm-2.9/include/llvm/ADT/GraphTraits.h:
+
+/y/students/jpvarbed/llvm-2.9/include/llvm/ADT/SmallPtrSet.h:
+
+/y/students/jpvarbed/llvm-2.9/include/llvm/ADT/PointerIntPair.h:
+
+/y/students/jpvarbed/llvm-2.9/include/llvm/ADT/GraphTraits.h:
+
+/y/students/jpvarbed/llvm-2.9/include/llvm/ADT/SmallVector.h:
+
+/y/students/jpvarbed/llvm-2.9/include/llvm/ADT/STLExtras.h:
+
+/y/students/jpvarbed/llvm-2.9/include/llvm/Analysis/Dominators.h:
+
+/y/students/jpvarbed/llvm-2.9/include/llvm/Function.h:
+
+/y/students/jpvarbed/llvm-2.9/include/llvm/ADT/SmallPtrSet.h:
+
+/y/students/jpvarbed/llvm-2.9/include/llvm/Support/CFG.h:
+
+/y/students/jpvarbed/llvm-2.9/include/llvm/ADT/GraphTraits.h:
+
+/y/students/jpvarbed/llvm-2.9/include/llvm/Function.h:
+
+/y/students/jpvarbed/llvm-2.9/include/llvm/InstrTypes.h:
+
+/y/students/jpvarbed/llvm-2.9/include/llvm/Operator.h:
+
+/y/students/jpvarbed/llvm-2.9/include/llvm/Constants.h:
+
+/y/students/jpvarbed/llvm-2.9/include/llvm/ADT/APInt.h:
+
+/y/students/jpvarbed/llvm-2.9/include/llvm/ADT/APFloat.h:
+
+/y/students/jpvarbed/llvm-2.9/include/llvm/ADT/APInt.h:
+
+/y/students/jpvarbed/llvm-2.9/include/llvm/DerivedTypes.h:
+
+/y/students/jpvarbed/llvm-2.9/include/llvm/Type.h:
+
+/y/students/jpvarbed/llvm-2.9/include/llvm/ADT/GraphTraits.h:
+
+/y/students/jpvarbed/llvm-2.9/include/llvm/Support/Compiler.h:
+
+/y/students/jpvarbed/llvm-2.9/include/llvm/PassManagers.h:
+
+/y/students/jpvarbed/llvm-2.9/include/llvm/Pass.h:
+
+/y/students/jpvarbed/llvm-2.9/include/llvm/ADT/SmallPtrSet.h:
+
+/y/students/jpvarbed/llvm-2.9/include/llvm/ADT/DenseMap.h:
+
+/y/students/jpvarbed/llvm-2.9/include/llvm/Support/PrettyStackTrace.h:
+
+/y/students/jpvarbed/llvm-2.9/include/llvm/Analysis/LoopInfo.h:
+
+/y/students/jpvarbed/llvm-2.9/include/llvm/Constants.h:
+
+/y/students/jpvarbed/llvm-2.9/include/llvm/Instructions.h:
+
+/y/students/jpvarbed/llvm-2.9/include/llvm/InstrTypes.h:
+
+/y/students/jpvarbed/llvm-2.9/include/llvm/Function.h:
+
+/y/students/jpvarbed/llvm-2.9/include/llvm/BasicBlock.h:
+
+/y/students/jpvarbed/llvm-2.9/include/llvm/Support/Debug.h:
+
+/y/students/jpvarbed/llvm-2.9/include/llvm/Target/TargetData.h:
+
+/y/students/jpvarbed/llvm-2.9/include/llvm/Pass.h:
+
+/y/students/jpvarbed/llvm-2.9/include/llvm/ADT/SmallVector.h:
+
+/y/students/jpvarbed/llvm-object/include/llvm/Support/DataTypes.h:
+
+/y/students/jpvarbed/llvm-2.9/include/llvm/ADT/IndexedMap.h:
+
+/y/students/jpvarbed/hw1/lamppass/include/LAMPProfiling.h:
---
+++ trunk/hw2_pass/lib/sample/Debug+Profile/sample.d
@@ -0,0 +1,6 @@
+/y/students/jpvarbed/hw1/samplepass/lib/sample/Debug+Profile/sample.o \
+  /y/students/jpvarbed/hw1/samplepass/lib/sample/Debug+Profile/sample.d \
+  /y/students/jpvarbed/hw1/samplepass/lib/sample/Debug+Profile/sample.o:  \
+ sample.c /y/students/jpvarbed/hw1/samplepass/include/sample.h
+
+/y/students/jpvarbed/hw1/samplepass/include/sample.h:

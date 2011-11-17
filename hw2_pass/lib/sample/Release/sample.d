208cbdc4e02d05e10e72a0c0c3b521d346d47557
jpvarbed <jpvarbed@37f40823-ce63-ffd7-518b-180745758533>
2011-11-17 18:25:09 +0000

    added my hw2 as a start structure

---
+++ hw2_pass/lib/sample/Release/sample.d
@@ -0,0 +1,6 @@
+/y/students/jpvarbed/hw1/samplepass/lib/sample/Release/sample.o \
+  /y/students/jpvarbed/hw1/samplepass/lib/sample/Release/sample.d \
+  /y/students/jpvarbed/hw1/samplepass/lib/sample/Release/sample.o:  \
+ sample.c /y/students/jpvarbed/hw1/samplepass/include/sample.h
+
+/y/students/jpvarbed/hw1/samplepass/include/sample.h:

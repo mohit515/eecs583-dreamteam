f33f1edcd4ec229408398054227f5acc9c8a372a
matthew90 <matthew90@37f40823-ce63-ffd7-518b-180745758533>
2011-11-17 23:08:52 +0000

    changed stuff up

--- trunk/hw2_pass/lib/sample/Release/sample.d
+++
@@ -1,6 +0,0 @@
-/y/students/jpvarbed/hw1/samplepass/lib/sample/Release/sample.o \
-  /y/students/jpvarbed/hw1/samplepass/lib/sample/Release/sample.d \
-  /y/students/jpvarbed/hw1/samplepass/lib/sample/Release/sample.o:  \
- sample.c /y/students/jpvarbed/hw1/samplepass/include/sample.h
-
-/y/students/jpvarbed/hw1/samplepass/include/sample.h:

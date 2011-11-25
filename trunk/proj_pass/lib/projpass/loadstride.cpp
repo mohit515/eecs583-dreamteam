f3bb0fceb76e7a3fb6773a7d652c499ec2a4880f
matthew90 <matthew90@37f40823-ce63-ffd7-518b-180745758533>
2011-11-25 21:09:04 +0000

    updated how to get top stride and differences

--- trunk/proj_pass/lib/projpass/loadstride.cpp
+++ trunk/proj_pass/lib/projpass/loadstride.cpp
@@ -4,6 +4,8 @@
 */
 
 #include <vector>
+#include <utility>
+#include <map>
 
 using namespace std;
 
@@ -23,15 +25,17 @@ class LoadStride {
       return strideZeroDifferenceCount;
     }
 
-    long *getTopStrideValues() {
+    map<long,long> *getTopStrideValues() {
       return topStrideValues;
     }
 
-    long *getTopStrideDifferenceValues() {
+    map<long,long> *getTopStrideDifferenceValues() {
       return topStrideDifferenceValues;
     }
     
   private:
+    static const int TOPCOUNT;
+
     vector<unsigned long> addresses;
     vector<long> strideValues;
     vector<long> strideDifferences;
@@ -40,9 +44,11 @@ class LoadStride {
     unsigned long strideZeroCount;
     unsigned long strideZeroDifferenceCount;
 
-    long topStrideValues[5];
-    long topStrideDifferenceValues[5];
+    vector< pair<long, long> > topStrideValues;
+    vector< pair<long, long> > topStrideDifferenceValues;
 }
+
+const int LoadStride::TOPCOUNT = 5; // number of top stride values to return
 
 LoadStride::LoadStride() {
   strideZeroCount = 0;
@@ -50,6 +56,46 @@ LoadStride::LoadStride() {
 }
 
 LoadStride::~LoadStride() {
+}
+
+void LoadStride::updateTopStrideValues(long value) {
+  long count = strideValuesToCount[value];
+  if (topStrideValues.size() < LoadStride::TOPCOUNT) {
+    topStrideValues.push_back(make_pair(value, count));
+    return;
+  }
+  map<long, long>::iterator itStart, itEnd;
+  long lowest_found = (topStrideValues.begin())->second;
+  int location = 0; 
+  for (int i = 1; i < topStrideValues.size(); i++) {
+    if (topStrideValues[i].second < lowest_found) {
+      lowest_found = topStrideValues[i].second;
+      location = i;
+    }
+  }
+  if (count > lowest_found) {
+    topStrideValues[location] = make_pair(value, count);
+  }
+}
+
+void LoadStride::updateTopStrideDifferenceValues(long value) {
+  long count = strideDifferencesToCount[value];
+  if (topStrideDifferenceValues.size() < LoadStride::TOPCOUNT) {
+    topStrideDifferenceValues.push_back(make_pair(value, count));
+    return;
+  }
+  map<long, long>::iterator itStart, itEnd;
+  long lowest_found = (topStrideDifferenceValues.begin())->second;
+  int location = 0; 
+  for (int i = 1; i < topStrideDifferenceValues.size(); i++) {
+    if (topStrideDifferenceValues[i].second < lowest_found) {
+      lowest_found = topStrideDifferenceValues[i].second;
+      location = i;
+    }
+  }
+  if (count > lowest_found) {
+    topStrideDifferenceValues[location] = make_pair(value, count);
+  }
 }
 
 void addAddress(unsigned long addr) {
@@ -78,6 +124,7 @@ void addAddress(unsigned long addr) {
   } else {
     strideValuesToCount[stride] = 1;
   }
+  updateTopStrideValues(stride);
 
   long strideDifference = stride - last_stride;
   if (strideDifference == 0) {
@@ -89,6 +136,7 @@ void addAddress(unsigned long addr) {
   } else {
     strideDifferencesToCount[strideDifference] = 1;
   }
+  updateTopStrideDifferenceValues(strideDifference);
 }
 
 

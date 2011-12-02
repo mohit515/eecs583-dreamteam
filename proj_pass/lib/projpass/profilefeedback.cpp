9ecd08a8b415441c57b95d79625dc1a4d7510a75
jpvarbed <jpvarbed@37f40823-ce63-ffd7-518b-180745758533>
2011-12-02 01:21:44 +0000

    added just short of instr

--- proj_pass/lib/projpass/profilefeedback.cpp
+++ proj_pass/lib/projpass/profilefeedback.cpp
@@ -7,18 +7,31 @@ using namespace llvm;
 #define SSST_T 10
 #define PMST_T 10
 #define WSST_T 10
+#define MAXPREFETCHDISTANCE 8
 
 set <Instruction*> SSST_loads;
 set <Instruction*> PMST_loads;
 set <Instruction*> WSST_loads;
 map <Instruction*, loadInfo> instToInfo;
 
+instToInfo getInfo(Instruction* inst)
+{
+    map <Instruction*, loadInfo>::iterator findInfo;
+    findInfo = instToInfo.find(inst);
+    if(findInfo == instToInfo.end()){
+        errs() << "couldnt find " << *inst << " in getInfo\n";
+    }
+    return findInfo->second;
+}
 struct instToInfo{
     int freq1;
     int freq2;
     int freq3;
     int freq4;
     int num_zero_diff;
+    int profiledStride;
+    int dominantStride;
+    int tripCount;
 };
 
 void profile(set<Instruction*>::iterator IT, set<Instruction*>::iterator ITe )
@@ -62,9 +75,47 @@ void profile(set<Instruction*>::iterator IT, set<Instruction*>::iterator ITe )
         }
     }
 }
-void insertLoad()
+//insert just prefetch(P+K*S)
+void insertSSST(Instruction *inst, double K)
 {
+}
+//insert an alloca to hold address
+//insert an alloca to hold stride
+//insert a subtract stride = addr(load) - scratch
+//scratch = addr(load)
+//prefetch(addr(load)+K*stride) before the load, K is rounded to a power of 2, 
+void insertPMST(Instruction *inst, double K)
+{
+}
+//insert an alloca to hold address
+//insert an alloca to hold stride
+//insert a subtract stride = addr(load) - scratch
+//scratch = addr(load)
+//p=(stride==profiled stride)
+//p?prefetch(P+K*stride)
 
+void insertWSST(Instruction *inst, double K)
+{
+}
+void insertLoad(Instruction *inst)
+{
+   //determine K
+   double K = 0;
+   instInfo profData = getInfo(inst);
+   double dataArea = profData.dominantStride * profData.tripCount;
+   double C = MAXPREFETCHDISTANCE;
+   K = min(profData.tripCount/TT, C);
+   //we can incorporate cache stuff if need be
+   //call the corresponding load list
+   set <Instruction*>::iterator loadIT;
+   if(PMST_loads.count(inst))
+       insertPMST(inst,K);
+   else if(SSST_loads.count(inst))
+       insertSSST(inst,K);
+   else if(WSST_loads.count(inst))
+       insertWSST(inst, K);
+   else
+       errs() << "inst not inserted\n";
 }
 int main()
 {

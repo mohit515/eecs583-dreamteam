0c465949716a37abfaa5177a811795074d8ec462
matthew90 <matthew90@37f40823-ce63-ffd7-518b-180745758533>
2011-12-04 02:48:34 +0000

    added StrideProfile:: to methods

--- trunk/proj_pass/lib/projpass/strideprefetch.cpp
+++ trunk/proj_pass/lib/projpass/strideprefetch.cpp
@@ -1,14 +1,14 @@
 /*
-  Stride Profiling & Prefetching
+   Stride Profiling & Prefetching
 
-  EECS 583 - Project
-  Jason Varbedian, Patryk Mastela, Matt Viscomi
+   EECS 583 - Project
+   Jason Varbedian, Patryk Mastela, Matt Viscomi
 
-  Based off of:
-    Efficient Discovery of Regular Stride Patterns in Irregular
-    Programs and Its Use in Compiler Prefetching
-      by Youfeng Wu
-*/
+   Based off of:
+   Efficient Discovery of Regular Stride Patterns in Irregular
+   Programs and Its Use in Compiler Prefetching
+   by Youfeng Wu
+ */
 
 #include "llvm/Transforms/Scalar.h"
 #include "llvm/Constants.h"
@@ -42,43 +42,43 @@ namespace llvm {
 namespace {
   struct StridePrefetch : public LoopPass {
 
-      static char ID;
-      
-      StridePrefetch() : LoopPass(ID) {
-        initializeStridePrefetchPass(*PassRegistry::getPassRegistry());
-      }
+    static char ID;
 
-      virtual bool runOnLoop(Loop *L, LPPassManager &LPM);
+    StridePrefetch() : LoopPass(ID) {
+      initializeStridePrefetchPass(*PassRegistry::getPassRegistry());
+    }
 
-      /// This transformation requires natural loop information & requires that
-      /// loop preheaders be inserted into the CFG...
-      ///
-      virtual void getAnalysisUsage(AnalysisUsage &AU) const {
-        AU.addRequired<DominatorTree>();
-        AU.addRequired<LoopInfo>();
-        AU.addRequired<ProfileInfo>();
-        AU.addRequired<AliasAnalysis>();
-        AU.addRequired<LAMPLoadProfile>();
-      }
+    virtual bool runOnLoop(Loop *L, LPPassManager &LPM);
+
+    /// This transformation requires natural loop information & requires that
+    /// loop preheaders be inserted into the CFG...
+    ///
+    virtual void getAnalysisUsage(AnalysisUsage &AU) const {
+      AU.addRequired<DominatorTree>();
+      AU.addRequired<LoopInfo>();
+      AU.addRequired<ProfileInfo>();
+      AU.addRequired<AliasAnalysis>();
+      AU.addRequired<LAMPLoadProfile>();
+    }
     private:
-      AliasAnalysis *AA;
-      LoopInfo *LI;
-      DominatorTree *DT;
-      ProfileInfo *PI;
-      LAMPLoadProfile *LP;
+    AliasAnalysis *AA;
+    LoopInfo *LI;
+    DominatorTree *DT;
+    ProfileInfo *PI;
+    LAMPLoadProfile *LP;
 #if FALSE
-      set <Instruction*> SSST_loads;
-      set <Instruction*> PMST_loads;
-      set <Instruction*> WSST_loads;
+    set <Instruction*> SSST_loads;
+    set <Instruction*> PMST_loads;
+    set <Instruction*> WSST_loads;
 
-      loadInfo getInfo(Instruction* inst);
-      void profile(Instruction* inst);
-      BinaryOperator *scratchAndSub(Instruction *inst);
-      void insertPrefetch(Instruction *inst, double K, BinaryOperator *sub);
-      void insertSSST(Instruction *inst, double K);
-      void insertPMST(Instruction *inst, double K);
-      void insertWSST(Instruction *inst, double K);
-      void insertLoad(Instruction *inst);
+    loadInfo getInfo(Instruction* inst);
+    void profile(Instruction* inst);
+    BinaryOperator *scratchAndSub(Instruction *inst);
+    void insertPrefetch(Instruction *inst, double K, BinaryOperator *sub);
+    void insertSSST(Instruction *inst, double K);
+    void insertPMST(Instruction *inst, double K);
+    void insertWSST(Instruction *inst, double K);
+    void insertLoad(Instruction *inst);
 #endif
   };
 }
@@ -90,154 +90,147 @@ namespace llvm {
 }
 
 char StridePrefetch::ID = 0;
-INITIALIZE_PASS_BEGIN(StridePrefetch, "strideprefetch", "Stride Prefetching", false, false)
-INITIALIZE_PASS_DEPENDENCY(DominatorTree)
-INITIALIZE_PASS_DEPENDENCY(LoopInfo)
-INITIALIZE_PASS_DEPENDENCY(LoopSimplify)
+  INITIALIZE_PASS_BEGIN(StridePrefetch, "strideprefetch", "Stride Prefetching", false, false)
+  INITIALIZE_PASS_DEPENDENCY(DominatorTree)
+  INITIALIZE_PASS_DEPENDENCY(LoopInfo)
+  INITIALIZE_PASS_DEPENDENCY(LoopSimplify)
 INITIALIZE_AG_DEPENDENCY(AliasAnalysis)
-INITIALIZE_PASS_END(StridePrefetch, "strideprefetch", "Stride Prefetching", false, false)
-static RegisterPass<StridePrefetch> X("projpass", "LICM Pass", true, true);
+  INITIALIZE_PASS_END(StridePrefetch, "strideprefetch", "Stride Prefetching", false, false)
+  static RegisterPass<StridePrefetch> X("projpass", "LICM Pass", true, true);
 
-bool StridePrefetch::runOnLoop(Loop *L, LPPassManager &LPM) {
+  bool StridePrefetch::runOnLoop(Loop *L, LPPassManager &LPM) {
     return false;
-}
+  }
 
 #if FALSE
-loadInfo getInfo(Instruction* inst)
-{
-    map <Instruction*, loadInfo>::iterator findInfo;
-    findInfo = LoadToLoadInfo.find(inst);
-    if(findInfo == LoadToLoadInfo.end()){
-        errs() << "couldnt find " << *inst << " in getInfo\n";
-    }
-    return findInfo->second;
+loadInfo StridePrefetch::getInfo(Instruction* inst) {
+  map <Instruction*, loadInfo>::iterator findInfo;
+  findInfo = LoadToLoadInfo.find(inst);
+  if(findInfo == LoadToLoadInfo.end()){
+    errs() << "couldnt find " << *inst << " in getInfo\n";
+  }
+  return findInfo->second;
 }
 
 
-void profile(Instruction *inst)
-{
-    int freq1 = 0;
-    int num_strides = 0;
-    int top_4_freq = 0;
-    loadInfo profData = 0;
-    int zeroDiff = 0;
-    
-    profData = getInfo(inst);
-        
-    if(PI->getExecutionCount(inst->getParent()) =< FT)
-        continue;
-    //assume that loads passed in are in loops
-    if(PI->getExecutionCount(Preheader) =< TT)
-        continue;
-    freq1 = profData.freq[0]
+void StridePrefetch::profile(Instruction *inst) {
+  int freq1 = 0;
+  int num_strides = 0;
+  int top_4_freq = 0;
+  loadInfo profData = 0;
+  int zeroDiff = 0;
+
+  profData = getInfo(inst);
+
+  if(PI->getExecutionCount(inst->getParent()) =< FT)
+    continue;
+  //assume that loads passed in are in loops
+  if(PI->getExecutionCount(Preheader) =< TT)
+    continue;
+  freq1 = profData.freq[0]
     num_strides = profData.num_strides;
-    for(unsigned int i = 0; i < profData.freq.size(); i++)
-        top_4_freq+=profData.top_freqs[i];
-    zeroDiff = profData.num_zero_diff;
-    //cache line stuff...not sure yet?
-    if(freq1/num_strides > SSST_T){
-        SSST_loads.insert(inst);
-        printf("adding to SSST\n");
-    }
-    else if((top_4_freq/num_strides > PMST_T) && zeroDiff/num_strides > PMST_T){
-        PMST_loads.insert(inst);
-        printf("adding to PMST\n");
-    }
-    else if((freq1/num_strides > WSST_T) && (zeroDiff/num_strides > PMST_T))
-    {
-        WSST_loads.insert(inst);
-        printf("adding to WSST\n");
-    }
-    else{
-        printf("adding to none\n");
-    }
+  for(unsigned int i = 0; i < profData.freq.size(); i++)
+    top_4_freq+=profData.top_freqs[i];
+  zeroDiff = profData.num_zero_diff;
+  //cache line stuff...not sure yet?
+  if(freq1/num_strides > SSST_T){
+    SSST_loads.insert(inst);
+    printf("adding to SSST\n");
+  }
+  else if((top_4_freq/num_strides > PMST_T) && zeroDiff/num_strides > PMST_T){
+    PMST_loads.insert(inst);
+    printf("adding to PMST\n");
+  }
+  else if((freq1/num_strides > WSST_T) && (zeroDiff/num_strides > PMST_T))
+  {
+    WSST_loads.insert(inst);
+    printf("adding to WSST\n");
+  }
+  else{
+    printf("adding to none\n");
+  }
 }
 //insert an alloca to hold address
 //insert an alloca to hold stride
 //insert a subtract stride = addr(load) - scratch
 //scratch = addr(load)
-BinaryOperator *scratchAndSub(Instruction *inst)
-{
-    AllocaInst *scratchPtr;
-    StoreInst *storePtr;
-    BinaryOperator *subPtr;
-    //inst->bb->fcn->module
-    Module *M = inst->getParent()->getParent()->getParent();
-    BasicBlock *entryBlock = inst->getParent()->getParent()->getEntryBlock().begin();
-    
-    StringRef X = "scratch";
-    StringRef Name = inst->getName();
-    //make alloca for scratch reg
-    Value *loadAddr = inst->getPointerOperand();
-    allocaPtr = new AllocaInst(loadAddr->getType(), 
-            X+Name, entryBlock); 
+BinaryOperator* StridePrefetch::scratchAndSub(Instruction *inst) {
+  AllocaInst *scratchPtr;
+  StoreInst *storePtr;
+  BinaryOperator *subPtr;
+  //inst->bb->fcn->module
+  Module *M = inst->getParent()->getParent()->getParent();
+  BasicBlock *entryBlock = inst->getParent()->getParent()->getEntryBlock().begin();
 
-    //store[scratchPtr] = loadAddr
-    storePtr = new StoreInst(scratchPtr, loadAddr, inst);
-    
-    //stride = addr(load) - scratch
-    subPtr = BinaryOperator::Create(Instruction::Sub,
-            loadAddr,
-            scratchPtr,
-            "stride",
-            inst
-            );
-    return subPtr;
+  StringRef X = "scratch";
+  StringRef Name = inst->getName();
+  //make alloca for scratch reg
+  Value *loadAddr = inst->getPointerOperand();
+  allocaPtr = new AllocaInst(loadAddr->getType(), 
+      X+Name, entryBlock); 
+
+  //store[scratchPtr] = loadAddr
+  storePtr = new StoreInst(scratchPtr, loadAddr, inst);
+
+  //stride = addr(load) - scratch
+  subPtr = BinaryOperator::Create(Instruction::Sub,
+      loadAddr,
+      scratchPtr,
+      "stride",
+      inst
+      );
+  return subPtr;
 }
 
 //inserts prefetch(addr(inst)+sub*K)
-void insertPrefetch(Instruction *inst, double K, BinaryOperator *sub)
-{
+void StridePrefetch::insertPrefetch(Instruction *inst, double K, BinaryOperator *sub) {
 }
+
 //insert just prefetch(P+K*S)
-void insertSSST(Instruction *inst, double K)
-{
-    insertPrefetch(inst, K, NULL);
+void StridePrefetch::insertSSST(Instruction *inst, double K) {
+  insertPrefetch(inst, K, NULL);
 }
 
 //scratch sub
 //prefetch(addr(load)+K*stride) before the load, K is rounded to a power of 2, 
-void insertPMST(Instruction *inst, double K)
-{
-    BinaryOperator *subPtr = scratchAndSub(inst);
-    insertPrefetch(inst, K, subPtr);
+void StridePrefetch::insertPMST(Instruction *inst, double K) {
+  BinaryOperator *subPtr = scratchAndSub(inst);
+  insertPrefetch(inst, K, subPtr);
 }
 
 //scratch sub
 //p=(stride==profiled stride)
 //p?prefetch(P+K*stride)
-void insertWSST(Instruction *inst, double K)
-{
-    BinaryOperator *subPtr = scratchAndSub(inst);
-    ICmpInst *ICmpPtr;
-    loadInfo profData = getInfo(inst);
-    int profiled_stride = profData.profiled_stride;
-    ICmpPtr = new ICmpInst(inst, 
-            ICmpInst::ICMP_EQ,
-            subPtr,
-            Constant::get(inst->getPointerOperand->getType(), profiled_stride), 
-            "cmpweak");
-    insertPrefetch(inst, K, subPtr);
+void StridePrefetch::insertWSST(Instruction *inst, double K) {
+  BinaryOperator *subPtr = scratchAndSub(inst);
+  ICmpInst *ICmpPtr;
+  loadInfo profData = getInfo(inst);
+  int profiled_stride = profData.profiled_stride;
+  ICmpPtr = new ICmpInst(inst, 
+      ICmpInst::ICMP_EQ,
+      subPtr,
+      Constant::get(inst->getPointerOperand->getType(), profiled_stride), 
+      "cmpweak");
+  insertPrefetch(inst, K, subPtr);
 }
 
-void insertLoad(Instruction *inst)
-{
-   //determine K
-   double K = 0;
-   loadInfo profData = getInfo(inst);
-   double dataArea = profData.dominant_stride * profData.trip_count;
-   double C = MAXPREFETCHDISTANCE;
-   K = min(profData.trip_count/TT, C);
-   //we can incorporate cache stuff if need be
-   //call the corresponding load list
-   set <Instruction*>::iterator loadIT;
-   if(PMST_loads.count(inst))
-       insertPMST(inst,K);
-   else if(SSST_loads.count(inst))
-       insertSSST(inst,K);
-   else if(WSST_loads.count(inst))
-       insertWSST(inst, K);
-   else
-       errs() << "inst not inserted\n";
+void StridePrefetch::insertLoad(Instruction *inst) {
+  //determine K
+  double K = 0;
+  loadInfo profData = getInfo(inst);
+  double dataArea = profData.dominant_stride * profData.trip_count;
+  double C = MAXPREFETCHDISTANCE;
+  K = min(profData.trip_count/TT, C);
+  //we can incorporate cache stuff if need be
+  //call the corresponding load list
+  set <Instruction*>::iterator loadIT;
+  if(PMST_loads.count(inst))
+    insertPMST(inst,K);
+  else if(SSST_loads.count(inst))
+    insertSSST(inst,K);
+  else if(WSST_loads.count(inst))
+    insertWSST(inst, K);
+  else
+    errs() << "inst not inserted\n";
 }
 #endif

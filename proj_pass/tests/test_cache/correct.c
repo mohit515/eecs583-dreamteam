80929b75d5da9024a134bb284f44f3ccaa542cb7
jpvarbed <jpvarbed@37f40823-ce63-ffd7-518b-180745758533>
2011-12-16 20:50:20 +0000

    small changes

--- proj_pass/tests/test_cache/correct.c
+++ proj_pass/tests/test_cache/correct.c
@@ -11,18 +11,26 @@
 struct timeval t_start, t_end;
 double t_diff;
 
+
   int i, j, k;
 int main(int argc, char* argv[]) {
   if (argc < 2) {
     printf("Put a fucking size!\n");
     return 0;
   }
+  int h = 0;
   int SIZE = atoi(argv[1]);
 
-  int A[SIZE][SIZE];
+  /*int A[SIZE][SIZE];
   int B[SIZE][SIZE];
-
-  
+  */
+  int **A = NULL, **B = NULL;
+  A = (int**)malloc(SIZE * sizeof(int*));
+  B = (int**)malloc(SIZE * sizeof(int*));
+  for(h = 0; h < SIZE; h++){
+    A[h] = (int*)malloc(SIZE *sizeof(int));
+    B[h] = (int*)malloc(SIZE *sizeof(int));
+  }
   // fill in the matrices
   for (i = 0; i < SIZE; i++) {
     for (j = 0; j < SIZE; j++) {
@@ -49,6 +57,13 @@ int main(int argc, char* argv[]) {
   gettimeofday(&t_end,NULL);
   t_diff = (t_end.tv_sec - t_start.tv_sec) + (double)(t_end.tv_usec - t_start.tv_usec)/1000000;
   printf(" ---  time spent = %.6f  --- \n", t_diff);
+
+  for(h = 0; h < SIZE; h++){
+    free(A[h]);
+    free(B[h]);
+  }
+  free(A);
+  free(B);
 
   return 0;
 }
